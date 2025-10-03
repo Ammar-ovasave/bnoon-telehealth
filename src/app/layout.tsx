3b9620566923f54cfff27a61ca32d23ba1fd1942
Ammar <ammar@ovasave.com>
2025-10-03 15:07:27 +0400

    update clinics list layout and add bnoon doctors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,30 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Bnoon",
   description: "Bnoon",
 };
+
+const helvetica = localFont({
+  src: [
+    {
+      path: "./fonts/Helvetica/Helvetica.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Helvetica/Helvetica-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Helvetica/helvetica-light-587ebe5a59211.ttf",
+      weight: "300",
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased ${helvetica.className}`}>{children}</body>
     </html>
   );
 }

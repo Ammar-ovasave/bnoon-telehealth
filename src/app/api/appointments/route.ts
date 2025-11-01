1c0094c404ab21623eeef9965b1a73dac12b6e3b
Ammar <ammar@ovasave.com>
2025-11-01 18:28:06 +0400

    remove google apis code

--- src/app/api/appointments/route.ts
+++ src/app/api/appointments/route.ts
@@ -1,5 +1,4 @@
 import { CreateAppointmentPayload } from "@/models/CreateAppointmentPayload";
-import { google } from "googleapis";
 import axios from "@/services/axios";
 
 export async function POST(request: Request) {
@@ -10,13 +9,13 @@ export async function POST(request: Request) {
       console.log("--- create appointment error", createAppointmentResponse.data);
       return Response.error();
     }
-    await createGoogleMeet({
-      appointmentId: createAppointmentResponse.data.id.toString(),
-      endDate: payload.endTime,
-      serviceName: "Test",
-      startDate: payload.startTime,
-      userEmail: payload.email,
-    });
+    // await createGoogleMeet({
+    //   appointmentId: createAppointmentResponse.data.id.toString(),
+    //   endDate: payload.endTime,
+    //   serviceName: "Test",
+    //   startDate: payload.startTime,
+    //   userEmail: payload.email,
+    // });
     return Response.json(createAppointmentResponse.data);
   } catch (error) {
     console.log("--- create appointment error", error);
@@ -24,70 +23,70 @@ export async function POST(request: Request) {
   }
 }
 
-function getCalendarClient() {
-  try {
-    const auth = new google.auth.GoogleAuth({
-      keyFile: "bnoon-476311-149de792a5ff.json",
-      scopes: ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"],
-    });
-    const calendar = google.calendar({ version: "v3", auth });
-    return { calendar, auth };
-  } catch (error) {
-    console.log("--- getCalendarClient error", error);
-    return null;
-  }
-}
+// function getCalendarClient() {
+//   try {
+//     const auth = new google.auth.GoogleAuth({
+//       keyFile: "bnoon-476311-149de792a5ff.json",
+//       scopes: ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"],
+//     });
+//     const calendar = google.calendar({ version: "v3", auth });
+//     return { calendar, auth };
+//   } catch (error) {
+//     console.log("--- getCalendarClient error", error);
+//     return null;
+//   }
+// }
 
-async function createGoogleMeet({
-  userEmail,
-  endDate,
-  startDate,
-  serviceName,
-  appointmentId,
-}: {
-  appointmentId: string;
-  serviceName: string;
-  userEmail: string;
-  startDate: string;
-  endDate: string;
-}) {
-  try {
-    const result = getCalendarClient();
-    if (!result) return null;
-    const { auth, calendar } = result;
-    if (!calendar) {
-      return null;
-    }
-    const response = await calendar.events.insert({
-      calendarId: "primary",
-      auth: auth,
-      requestBody: {
-        summary: serviceName,
-        description: "Teleconsultation",
-        start: {
-          dateTime: startDate,
-          timeZone: "Asia/Dubai",
-        },
-        end: {
-          dateTime: endDate,
-          timeZone: "Asia/Dubai",
-        },
-        attendees: [{ email: userEmail }],
-        anyoneCanAddSelf: true,
-        conferenceData: {
-          createRequest: {
-            requestId: appointmentId,
-            conferenceSolutionKey: { type: "hangoutsMeet" },
-          },
-        },
-      },
-      conferenceDataVersion: 1,
-      sendUpdates: "all",
-    });
-    console.log("✅ Event created:");
-    console.log("Meeting Link:", JSON.stringify(response));
-  } catch (error) {
-    console.log("--- create google meet error", error);
-    return null;
-  }
-}
+// async function createGoogleMeet({
+//   userEmail,
+//   endDate,
+//   startDate,
+//   serviceName,
+//   appointmentId,
+// }: {
+//   appointmentId: string;
+//   serviceName: string;
+//   userEmail: string;
+//   startDate: string;
+//   endDate: string;
+// }) {
+//   try {
+//     const result = getCalendarClient();
+//     if (!result) return null;
+//     const { auth, calendar } = result;
+//     if (!calendar) {
+//       return null;
+//     }
+//     const response = await calendar.events.insert({
+//       calendarId: "primary",
+//       auth: auth,
+//       requestBody: {
+//         summary: serviceName,
+//         description: "Teleconsultation",
+//         start: {
+//           dateTime: startDate,
+//           timeZone: "Asia/Dubai",
+//         },
+//         end: {
+//           dateTime: endDate,
+//           timeZone: "Asia/Dubai",
+//         },
+//         attendees: [{ email: userEmail }],
+//         anyoneCanAddSelf: true,
+//         conferenceData: {
+//           createRequest: {
+//             requestId: appointmentId,
+//             conferenceSolutionKey: { type: "hangoutsMeet" },
+//           },
+//         },
+//       },
+//       conferenceDataVersion: 1,
+//       sendUpdates: "all",
+//     });
+//     console.log("✅ Event created:");
+//     console.log("Meeting Link:", JSON.stringify(response));
+//   } catch (error) {
+//     console.log("--- create google meet error", error);
+//     return null;
+//   }
+// }

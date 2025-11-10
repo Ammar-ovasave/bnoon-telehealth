77bb80be134f6ea202e48fcb77c8d2ffb0e753b0
Ammar <ammar@ovasave.com>
2025-11-10 12:22:01 +0400

    replace services api with api services api

--- src/hooks/useFertiSmartAPIServices.ts
+++ src/hooks/useFertiSmartAPIServices.ts
@@ -2,9 +2,7 @@ import useSWR from "swr";
 import { FertiSmartAPIServiceModel } from "../models/FertiSmartAPIServiceModel";
 
 export default function useFertiSmartAPIServices() {
-  const { data, error, isLoading } = useSWR<FertiSmartAPIServiceModel[]>(
-    `/api/ferti-smart/appointment-services?activeOnly=false`
-  );
+  const { data, error, isLoading } = useSWR<FertiSmartAPIServiceModel[]>(`/api/ferti-smart/api-services`);
 
   return { data, error, isLoading };
 }

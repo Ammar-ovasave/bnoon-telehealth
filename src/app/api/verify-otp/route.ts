56ecbcb030ec7641f43a90815b793b244ece58be
Ammar <ammar@ovasave.com>
2025-11-14 14:22:21 +0400

    new otp endpoint

--- src/app/api/verify-otp/route.ts
+++ src/app/api/verify-otp/route.ts
@@ -1,4 +1,3 @@
-import axios from "@/services/axios";
 import { cookies } from "next/headers";
 import { AUTH_TOKEN_NAME } from "@/constants";
 import { getPatient } from "@/services/appointment-services";
@@ -8,12 +7,17 @@ export async function POST(request: Request) {
   try {
     const cookieStore = await cookies();
     const baseAPIURL = cookieStore.get("branchAPIURL")?.value;
+    const otpCode = cookieStore.get("otpCode")?.value;
+    if (!otpCode) {
+      return Response.error();
+    }
     const payload: { code?: string; purpose?: string; mrn?: string } = await request.json();
     if (!payload.code || !payload.mrn || !payload.purpose) {
       console.log("--- verify otp invalid payload", payload);
       return Response.error();
     }
     const res = await verifyOTP({
+      otpCode: otpCode,
       code: payload.code,
       mrn: payload.mrn,
       purpose: payload.purpose,
@@ -48,22 +52,28 @@ async function verifyOTP({
   mrn,
   code,
   purpose,
+  otpCode,
   baseAPIURL,
 }: {
+  otpCode: string;
   mrn: string;
   purpose: string;
   code: string;
   baseAPIURL: string | null;
 }) {
   try {
-    const res = await axios.post<{
-      verified?: boolean;
-    }>(baseAPIURL ? `${baseAPIURL}/patients/${mrn}/otps:verify` : `/patients/${mrn}/otps:verify`, { code, purpose });
-    if (!res.data.verified) {
-      console.log("--- verify otp error response", res.status, res.data);
-      return null;
+    if (code === otpCode) {
+      return { verified: true };
     }
-    return res.data;
+    return { verified: false };
+    // const res = await axios.post<{
+    //   verified?: boolean;
+    // }>(baseAPIURL ? `${baseAPIURL}/patients/${mrn}/otps:verify` : `/patients/${mrn}/otps:verify`, { code, purpose });
+    // if (!res.data.verified) {
+    //   console.log("--- verify otp error response", res.status, res.data);
+    //   return null;
+    // }
+    // return res.data;
   } catch (error) {
     console.log("--- verifyOTP error", { mrn, code, purpose, baseAPIURL }, error);
     return null;

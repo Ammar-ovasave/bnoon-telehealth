19f5026618fccccb6fa334c0a51626a3dd0561a9
Ammar <ammar@ovasave.com>
2025-12-15 23:19:21 +0400

    commit

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,13 +1,13 @@
-import { initializeApp, cert, App } from 'firebase-admin/app';
+import { initializeApp, cert, App, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = require('./bnoon-476311-cf2969a052a4.json')
+import serviceAccount from './bnoon-476311-cf2969a052a4.json';
 
 export let firebaseApp: App | null = null;
 
 if (!firebaseApp) {
   firebaseApp = initializeApp({
-    credential: cert(serviceAccount)
+    credential: cert(serviceAccount as ServiceAccount)
   });
 }
 

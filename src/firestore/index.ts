a019e3566731a29c831cd77446893afecf412a94
Ammar <ammar@ovasave.com>
2025-12-18 19:45:09 +0400

    update firebase key

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -2,7 +2,14 @@ import { initializeApp, cert, getApps, getApp, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 function getServiceAccount() {
-  return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT ?? "");
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    if (serviceAccount.private_key) {
+      serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+    }
+    return serviceAccount;
+  }
+  throw new Error("Firebase service account credentials not found. Please set FIREBASE_SERVICE_ACCOUNT environment variable.");
 }
 
 const serviceAccount = getServiceAccount();
